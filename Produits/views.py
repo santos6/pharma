aefe0edb8ef4894ad8a1ae4ae0a2356919c4d6ef
Santos YS <ratibus95@gmail.com>
2024-11-11 19:23:48 +0100

    Debut de l'affichage avec la class vues générique - CRUD

--- Produits/views.py
+++ Produits/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from .models import *
-
+from django.views.generic import ListView
 
 # La fonction 'home' permet d'afficher la page home.html
 def home(request):
     return render(request,'home.html')
+
+    #Récupération des données dans la base de données
+   # donnees = Produits.objects.all()
+
+   # context= {
+   #     'donnees' :donnees
+   # }
+
+   # return render(request,'home.html',context)
     
+
+class Affichage(ListView):
+    # Affichage du template
+    template_name = 'home.html'
+    #Récupération des données
+    queryset = Produits.objects.all()
+    
+ 
