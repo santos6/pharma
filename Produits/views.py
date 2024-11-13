ab6481c897a18bd029e134e3085fa5df26caf837
Santos YS <ratibus95@gmail.com>
2024-11-13 17:57:05 +0100

    Ok pour l'ajout des produits via le formulaire et le champs de la date d'expiration est de type 'date'

--- Produits/views.py
+++ Produits/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.views.generic import ListView
 
@@ -23,4 +23,23 @@ class Affichage(ListView):
     queryset = Produits.objects.all()
     
 def ajout_donnees(request):
-    return render(request,"ajout-donnees.html")
+    if request.method == 'POST':
+        name = request.POST['name']
+        #category = request.POST['category']
+        price = request.POST['price']
+        quantite = request.POST['quantite']
+        description = request.POST['description']
+        date_expiration = request.POST['date_expiration']
+        image = request.FILES['image']
+
+        #Récupération des catégories en fonction de la clé primaire
+        category = Categories.objects.get(pk = request.POST['category'])
+
+        savedonnees = Produits(name= name, price= price, quantite= quantite, description= description , date_expiration= date_expiration, category= category, image= image)
+        savedonnees.save()
+        return redirect('home')
+    
+    else:
+        category = Categories.objects.all
+
+    return render(request,"ajout-donnees.html", {"category" : category}) 
