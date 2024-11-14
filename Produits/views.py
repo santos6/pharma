a6e5f867a5e2d137a04fc3fe64fe832cc39e4e19
Santos YS <ratibus95@gmail.com>
2024-11-14 14:38:05 +0100

    formulaire avec un probleme de gestion des erreurs. Les erreurs ont été mis en commentaire dans le fichier views.py et ajout-donnees.html de façon temporelle

--- Produits/views.py
+++ Produits/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.views.generic import ListView
+from django.contrib import messages
+from datetime import datetime
 
 # La fonction 'home' permet d'afficher la page home.html
 def home(request):
@@ -23,6 +25,8 @@ class Affichage(ListView):
     queryset = Produits.objects.all()
     
 def ajout_donnees(request):
+    #Creation de la variable pour la gestion des erreus
+    errors = {}
     if request.method == 'POST':
         name = request.POST['name']
         #category = request.POST['category']
@@ -32,14 +36,42 @@ def ajout_donnees(request):
         date_expiration = request.POST['date_expiration']
         image = request.FILES['image']
 
-        #Récupération des catégories en fonction de la clé primaire
-        category = Categories.objects.get(pk = request.POST['category'])
+        #--- Validation de la date ---
+        #try :
+            #date_expiration= datetime.strftime(date_expiration, '%Y-%m-%d')
+        #except ValueError:
+            #errors ['date_expiration'] = "Le format de la date n'est pas bon. Essayez ceci: AAAA-MM-JJ"
+        
+        #--- Validation du prix ---
+        try:
+            price = float(price)
+            
+            if price < 0:
+                errors['price'] = "Le prix ne peut pas être négatif"
+        except ValueError:
+            errors ['price'] = "Entrez un prix valide svp"
 
-        savedonnees = Produits(name= name, price= price, quantite= quantite, description= description , date_expiration= date_expiration, category= category, image= image)
-        savedonnees.save()
+        #Si aucune erreur n'est contatée
+        if not errors:
+            try:
+                #Récupération des catégories en fonction de la clé primaire
+                category = Categories.objects.get(pk = request.POST['category'])
+
+                savedonnees = Produits(name= name, price= price, quantite= quantite, description= description , date_expiration= date_expiration, category= category, image= image)
+                savedonnees.save()
+                messages.success(request, "Le produit a été ajouté avec succès")
+            
+            except Categories.DoesNotExist: 
+                errors ['category'] = f'La catégorie spécifiée est introuvable.'
+            except KeyError as e:
+                errors [str(e)] = f'Le champ {e} est manquant dans la requête'
+            except Exception as e:
+                messages.error(request, f'Une erreur est survenu: {e}')
+
         return redirect('home')
     
     else:
         category = Categories.objects.all
 
-    return render(request,"ajout-donnees.html", {"category" : category}) 
+    return render(request,"ajout-donnees.html", {"category" : category }) 
+    #return render(request,"ajout-donnees.html", {"category" : category, "errors" : errors })
