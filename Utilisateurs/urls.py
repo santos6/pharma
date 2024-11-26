c2095d6524e8cef154aeaad4b1b2e0b031ab8939
Santos YS <ratibus95@gmail.com>
2024-11-26 09:35:27 +0100

    La modification du mot de passe fonctionne bien

--- Utilisateurs/urls.py
+++ Utilisateurs/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
   path('connecter/',Connecter_Compte, name='login'),
   path('creation/',Creation_Compte, name='creation'),
+  path('verification/',Verification_Mail, name='verification'),
+  path('modification-code/<str:email>/',Changement_Code, name='modifierCode'),
 
 ]
--- Utilisateurs/views.py
+++ Utilisateurs/views.py
@@ -71,3 +71,65 @@ def Connecter_Compte(request):
             messages.error(request,"Nom d'utilisateur ou  mot de passe incorrect")
             return redirect("login")
     return render(request,'login.html')
+
+
+#Fonction pour la vérification de l'adresse email
+
+def Verification_Mail(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+
+        # verification si l'email 
+        if not email:
+            messages.error(request,"Veuillez entrer une adresse mail valide.")
+            return(request,"verificationMail.html")
+        
+        user = User.objects.filter(email = email).first
+
+        if user:
+            return redirect("modifierCode",email=email)
+        else:
+            messages.error(request,"Cette adresse ne correspond à aucun compte. Veuillez réessayez avec une autre ou créez un compte")
+            return redirect("verification")
+
+    return render(request,"verificationMail.html")
+
+# fonction pour changer le mot de passe après vérification
+
+def Changement_Code(request, email):
+    try:
+        user =User.objects.get(email=email)
+
+    except User.DoesNotExist:
+        messages.error(request,"L'utilisateur Introuvable")
+        return redirect("login")
+    
+    if request.method == "POST":
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if password == password_confirm:
+          
+            if  len (password) < 8:
+              messages.error(request, "Le mot de passe doit contenir au moins 8 caractères.")
+              
+            elif not any(char.isdigit() for char in password):
+                messages.error(request, "Le mot de passe doit contenir au moins un chiffre")
+
+            elif not any(char.isalpha() for char in password):
+                messages.error(request,"Le mot de passe doit contenir au moins une lettre")
+            
+            else:
+                user.set_password(password)
+                user.save()
+                messages.success(request,"Le mot de passe a bien été modifier; Connectez-vous maintenant")
+
+                return redirect("login")
+
+        else:
+            messages.error(request,"Les mots de passe ne correspondent pas. Réessayez")
+
+    context = {
+    'email': email
+    }
+    return render(request,"nouveauMDP.html", context)
